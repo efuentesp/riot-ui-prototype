ad5b235dd013f1624bde656a7555ed0392cfb875
Edgar F. Fuentes Perea (EFP) <efuentes@softtek.com>
2018-08-24 15:17:14 -0500

    Se resolvió el problema para generar la carpeta de distribución.

--- webpack.config-helper.js
+++ webpack.config-helper.js
@@ -1,73 +1,78 @@
-'use strict'
+"use strict";
 
-const Path = require('path')
-const Webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const ExtractSASS = new ExtractTextPlugin('styles/bundle.css')
+const Path = require("path");
+const Webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractSASS = new ExtractTextPlugin("bundle.css");
 
-module.exports = (options) => {
+module.exports = options => {
   const webpackConfig = {
     devtool: options.devtool,
     entry: [
       `webpack-dev-server/client?http://localhost:${options.port}`,
-      'webpack/hot/dev-server',
-      './src/index'
+      "webpack/hot/dev-server",
+      "./src/index"
     ],
     output: {
-      path: Path.join(__dirname, 'dist'),
-      filename: 'bundle.js'
+      path: Path.join(__dirname, "dist"),
+      filename: "bundle.js"
     },
     plugins: [
       new Webpack.ProvidePlugin({
-        riot: 'riot'
+        riot: "riot"
       }),
       new Webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(options.isProduction ? 'production' : 'development')
+        "process.env": {
+          NODE_ENV: JSON.stringify(
+            options.isProduction ? "production" : "development"
+          )
         }
       }),
       new HtmlWebpackPlugin({
-        template: './src/index.html'
+        template: "./src/index.html"
       })
     ],
     module: {
-      preLoaders: [{
-        test: /\.tag$/,
-        exclude: /node_modules/,
-        include: /src/,
-        loader: 'riotjs-loader',
-        query: {
-          type: 'none'
+      preLoaders: [
+        {
+          test: /\.tag$/,
+          exclude: /node_modules/,
+          include: /src/,
+          loader: "riotjs-loader",
+          query: {
+            type: "none"
+          }
         }
-      }],
+      ],
       loaders: [
         {
           test: /\.js$|\.tag$/,
           exclude: /(node_modules|bower_components)/,
-          loader: 'babel',
+          loader: "babel",
           query: {
-            presets: ['es2015']
+            presets: ["es2015"]
           }
         },
         {
           test: /\.(png|jpg|gif|svg)$/,
-          loader: 'url-loader'
+          loader: "url-loader"
         },
         {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+          loader:
+            "url-loader?name=fonts/[name].[ext]&limit=10000&mimetype=application/font-woff"
         },
         {
           test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'file-loader'
+          loader: "file-loader?name=fonts/[name].[ext]"
         }
       ]
     }
-  }
+  };
 
   if (options.isProduction) {
-    webpackConfig.entry = ['./src/scripts/index']
+    webpackConfig.entry = ["./src/index"];
 
     webpackConfig.plugins.push(
       new Webpack.optimize.OccurenceOrderPlugin(),
@@ -77,34 +82,35 @@ module.exports = (options) => {
         }
       }),
       ExtractSASS
-    )
+    );
 
     webpackConfig.module.loaders.push({
       test: /\.scss$/i,
-      loader: ExtractSASS.extract(['css', 'sass'])
-    })
+      loader: ExtractSASS.extract(["css", "sass"])
+    });
   } else {
-    webpackConfig.plugins.push(
-      new Webpack.HotModuleReplacementPlugin()
-    )
+    webpackConfig.plugins.push(new Webpack.HotModuleReplacementPlugin());
 
-    webpackConfig.module.loaders.push({
-      test: /\.scss$/i,
-      loaders: ['style', 'css', 'sass']
-    }, {
-      test: /\.js$/,
-      loader: 'eslint',
-      exclude: /node_modules/
-    })
+    webpackConfig.module.loaders.push(
+      {
+        test: /\.scss$/i,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.js$/,
+        loader: "eslint",
+        exclude: /node_modules/
+      }
+    );
 
     webpackConfig.devServer = {
-      contentBase: './dist',
+      contentBase: "./dist",
       hot: true,
       port: options.port,
       inline: true,
       progress: true
-    }
+    };
   }
 
-  return webpackConfig
-}
+  return webpackConfig;
+};
